6bb0c19f4836052eb99f13a6f1e3e0e395e6f8ad
Leon Hartley <lhartley97@gmail.com>
2015-09-20 16:05:55 +0100

    Cleanup

--- OpenHabbo-PeerService/src/main/java/com/openhabbo/peer/networking/clients/ClientHandler.java
+++ OpenHabbo-PeerService/src/main/java/com/openhabbo/peer/networking/clients/ClientHandler.java
@@ -19,31 +19,31 @@ public class ClientHandler extends SimpleChannelInboundHandler<IncomingMessageWr
     private final AtomicInteger activeConnections = new AtomicInteger(0);
 
     @Override
-<<<<<<< HEAD
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, IncomingMessageWrapper wrapper) throws Exception {
         Session session = channelHandlerContext.channel().attr(SessionFactory.SESSION_ATTRIBUTE).get();
 
-        if(session == null) {
+        if (session == null) {
             // How do you expect us to handle the message if we aren't even a registered session? :P
             return;
         }
 
         session.handleEvent(wrapper.getHeader(), wrapper);
 
-        if(wrapper.getHeader() == 206) {
+        if (wrapper.getHeader() == 206) {
             channelHandlerContext.writeAndFlush("DAQBHHIIKHJIPAIQAdd-MM-yyyy\u0002SAHPB/client\u0002QBH\u0001");
-        } else if(wrapper.getHeader() == 415) {
+        } else if (wrapper.getHeader() == 415) {
             // sso ticket
             String ssoTicket = wrapper.readString();
 
             log.info("SSO ticket: {}", ssoTicket);
-        } else if(wrapper.getHeader() == 482) {
+        } else if (wrapper.getHeader() == 482) {
             log.info(wrapper.readString());
             log.info(wrapper.readString());
             log.info(wrapper.readString());
             log.info(wrapper.readString());
         }
-=======
+    }
+
     public void channelActive(ChannelHandlerContext ctx) throws java.lang.Exception {
         this.activeConnections.incrementAndGet();
 
@@ -53,11 +53,6 @@ public class ClientHandler extends SimpleChannelInboundHandler<IncomingMessageWr
             // disconnect if the session was not created successfully.
             ctx.close();
         }
-    }
-
-    @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, IncomingMessageWrapper wrapper) throws Exception {
->>>>>>> c5c278df3a8fdfc485017c823e5504594d060923
     }
 
     public static ChannelHandler getInstance() {
